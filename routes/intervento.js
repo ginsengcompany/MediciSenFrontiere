0f6defab5914e9ed1d972cd3be61bb0338f0ca5b
Antonio Valente <antosaverio.valente@gmail.com>
2017-11-07 09:45:25 +0100

    aggiungo pagina intervento

--- routes/intervento.js
+++ routes/intervento.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET dati page. */
+/* GET intervento page. */
 router.get('/', function(req, res, next) {
-    res.render('dati', { title: 'Express' });
+    res.render('intervento', { title: 'Express' });
 });
 
 module.exports = router;
