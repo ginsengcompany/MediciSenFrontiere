8d9f7b436142b336281fb36ffc6d01bb819b9594
Antonio Valente <antosaverio.valente@gmail.com>
2017-12-14 15:44:12 +0100

    modifiche allo stile

--- public/assets/js/controllers/indexController.js
+++ public/assets/js/controllers/indexController.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $.notify({
-        icon: 'pe-7s-gift',
-        message: "Benvenuto in  <b>Medici Senza Frontiere</b>"
+        icon: 'pe-7s-smile',
+        message: "Benvenuto in  <b>Surgery For Children</b>"
     },{
         type: 'info',
         timer: 4000
